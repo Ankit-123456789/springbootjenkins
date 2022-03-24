327823bca0ea8259a60427c1cafcf2595721840a
Ankit <learncodewithdurgesh@gmail.com>
2022-03-24 16:12:24 +0530

    package structure

--- src/main/java/com/example/mysql/springboot/SpringbootApplication.java
+++ src/main/java/com/example/mysql/springboot/SpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.example.mysqlpo.springboot;
+package com.example.mysql.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/mysql/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/mysql/springboot/SpringbootApplicationTests.java
@@ -1,8 +1,6 @@
 package com.example.mysql.springboot;
 
-import com.example.mysqlpo.springboot.SpringbootApplication;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = SpringbootApplication.class)
