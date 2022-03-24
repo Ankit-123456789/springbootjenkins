14bfc974a917861e450a04c3b5d5a822753d73d3
Ankit <learncodewithdurgesh@gmail.com>
2022-03-24 16:04:07 +0530

    fifth commit

--- src/test/java/com/example/mysql/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/example/mysql/springboot/SpringbootApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.mysql.springboot;
 
+import com.example.mysqlpo.springboot.SpringbootApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringbootApplication.class)
 class SpringbootApplicationTests {
 
 	@Test
